801fb70e7c917e3c720fb72c5158f63de4fe9282
chi8708 <983143462@qq.com>
2020-06-20 14:00:28 +0800

    数据库表、视图、存储过程差异更新

--- 9.数据库信息比对.sql
+++ 9.数据库信息比对.sql
@@ -25,3 +25,28 @@ U = 用户表
 UQ = UNIQUE 约束（类型是 K） 
 V = 视图 
 X = 扩展存储过程
+
+
+---获取存储过程脚本
+declare @p_text varchar(max)
+SELECT @p_text= definition FROM sys.sql_modules 
+JOIN sys.objects ON sys.sql_modules.object_id=sys.objects.object_id --and type='P' 
+and sys.objects.name='ChangeMessageReadUser'
+print @p_text
+
+---刷新视图
+exec sp_refreshview @name   
+
+--比较两个表字段差异
+1.
+SELECT * FROM dlxd.[INFORMATION_SCHEMA].[COLUMNS] SC1 WHERE sc1.TABLE_NAME='pub_User'
+		EXCEPT
+		SELECT* FROM test.[INFORMATION_SCHEMA].[COLUMNS] SC2 WHERE sc2.TABLE_NAME='user1'
+
+
+2.
+select a.name columnname,c.name as typename,case when a.is_nullable =0 then 'Not Null' else 'Null' end as nullable,a.*
+from sys.columns a , sys.objects b, sys.types c 
+where a.object_id= b.object_id and b.name='Pub_User' and a.system_type_id=c.system_type_id 
+
+
