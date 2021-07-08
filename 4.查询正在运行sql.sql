10f720e6de812d2e02fd09da4f0f411903476066
chi8708 <983143462@qq.com>
2021-07-08 10:15:20 +0800

    修改正在运行的sql

--- 4.查询正在运行sql.sql
+++ 4.查询正在运行sql.sql
@@ -17,22 +17,8 @@ CROSS APPLY sys.dm_exec_sql_text(der.[sql_handle]) AS dest WHERE [session_id]>50
 KILL 318
 
 
---DBCC showcontig('Call_TelService_VW')
+--DBCC showcontig('Table1')
 
---DBCC DBREINDEX('Call_TelService_VW')
+--DBCC DBREINDEX('Table1')
 
-SELECT TOP 1000 * FROM dbo.Core_MaterialOrderProcessSubhis
 
---TRUNCATE TABLE Core_MaterialOrderProcessSubhis
-
-select convert(char(10),getdate(),120)
-
-SELECT
-o.name, i.ROWS
-FROM sysobjects o, sysindexes i
-WHERE o.id = i.id  AND o.Xtype = 'U' AND i.indid < 2
-ORDER BY i.ROWS DESC;
-
-SELECT TOP 100 * FROM Core_SettleFRMaterial WHERE createdate >'2016-1-1'
-
---TRUNCATE TABLE Call_KeyPress2017020301
