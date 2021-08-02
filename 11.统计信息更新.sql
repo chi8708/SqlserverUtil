c0aa295b1263f4b38c33f6f2f10c8bb11d04307c
chi8708 <983143462@qq.com>
2021-08-02 20:29:19 +0800

    修改统计信息

--- 11.统计信息更新.sql
+++ 11.统计信息更新.sql
@@ -30,3 +30,27 @@ DBCC SHOW_STATISTICS('Pub_Client',Address_index)
 
 
 参考：https://www.cnblogs.com/xinysu/p/6484788.html
+
+
+---方法2.更新整个库 对使用全文索引的表有用
+USE DB1
+Go 
+
+DECLARE @DBCCString NVARCHAR(1000)
+DECLARE @TableName VARCHAR(100)
+DECLARE Cur_Index CURSOR FOR  SELECT Name AS TblName FROM sysobjects  WHERE xType='U' ORDER BY TblName
+FOR READ ONLY
+OPEN Cur_Index
+FETCH NEXT FROM Cur_Index INTO @TableName
+WHILE @@FETCH_STATUS=0
+BEGIN
+   SET @DBCCString = 'update STATISTICS [' +@TableName +']'
+   print @DBCCString
+   EXEC SP_EXECUTESQL  @DBCCString
+   PRINT convert(char(20),getdate(),120)+ '更新表' + @TableName +'的统计信息........OK!'
+   waitfor delay'00:00:02'
+   FETCH NEXT FROM Cur_Index INTO @TableName
+END
+CLOSE Cur_Index
+DEALLOCATE Cur_Index
+PRINT '操作完成！'
