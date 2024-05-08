2477c5ace00dbe7b342d7526d3d5909b1180897c
chiDreal <chitongshang@outlook.com>
2024-05-08 15:10:07 +0800

    修改forxml连接字段

--- 6.字符串逗号分隔返回table.sql
+++ 6.字符串逗号分隔返回table.sql
@@ -11,3 +11,7 @@ ALTER  function   [dbo].[Dhf_split](@c   varchar(2000),@split   varchar(2))
        insert   @t(col)   values   (@c)   
        return   
      end 
+
+
+
+将多行某列值合并为逗号分隔的一列  SELECT STUFF((SELECT ','+name FROM 表名 for xml path('')),1,1,'')
