47108e939da24c93ffe24ddebf402903b5060759
chiDreal <chitongshang@outlook.com>
2024-06-05 17:35:54 +0800

    修改数据字典

--- 22.数据字典.sql
+++ 22.数据字典.sql
@@ -2,15 +2,15 @@
 SELECT
     t.[name] AS 表名,
     c.[name] AS 字段名,
+	CASE WHEN pkc.object_id IS NOT NULL THEN '√' ELSE '' END AS 是否主键,
+	CASE WHEN c.is_identity = 1 THEN '√' ELSE '' END AS 是否标识,
     st.name AS 字段类型,
     c.max_length AS 字段长度,
 	--CASE WHEN st.name IN ('decimal', 'numeric') THEN CONCAT(c.precision, ',', c.scale) ELSE CAST(c.max_length AS VARCHAR(10)) END AS 字段长度,
 	CASE WHEN st.name IN ('decimal', 'numeric') THEN CAST(c.scale AS NVARCHAR(5))  ELSE '' END AS 小数位数,
-   CASE when  c.is_nullable=1 THEN '是' ELSE '否'END AS 是否为空,
-    CASE WHEN c.is_identity = 1 THEN '是' ELSE '否' END AS 是否标识,
-    CASE WHEN pkc.object_id IS NOT NULL THEN '是' ELSE '否' END AS 是否主键,
-    dc.definition AS 默认值,
-    CAST(ep.[value] AS VARCHAR(100)) AS [字段说明]
+   CASE when  c.is_nullable=1 THEN '√' ELSE ''END AS 是否为空,
+   ISNULL(dc.definition,'')  AS 默认值,
+   ISNULL(CAST(ep.[value] AS VARCHAR(1000)),'')  AS [字段说明]
 FROM sys.tables AS t
 INNER JOIN sys.columns AS c ON t.object_id = c.object_id
 LEFT JOIN sys.extended_properties AS ep ON c.object_id = ep.major_id
@@ -20,7 +20,7 @@ LEFT JOIN sys.types AS st ON c.system_type_id = st.system_type_id
 LEFT JOIN sys.key_constraints AS pkc ON t.object_id = pkc.parent_object_id
     AND c.column_id IN (SELECT ic.column_id FROM sys.index_columns AS ic WHERE ic.object_id = t.object_id AND ic.index_id = pkc.unique_index_id)
 LEFT JOIN sys.default_constraints AS dc ON c.default_object_id = dc.object_id
-WHERE st.name<>'sysname'
+WHERE st.name<>'sysname' AND t.name='PX_ExamPageRecordAnswer'
 ORDER BY t.name, c.column_id;
 
 
